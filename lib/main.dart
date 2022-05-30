8388d5cd2a53125affea69292bdad2b726b15d9c
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2022-05-30 21:30:39 +0300

    labproject

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:project/pages/splash_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+  ));
+}
